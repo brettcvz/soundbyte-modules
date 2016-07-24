6a805b34927354bb6da2f12bfa3c108ed72ee9a5
Brett van Zuiden <brettcvz@gmail.com>
2016-07-23 21:05:20 -0700

    Updated examples

--- module_example.py
+++ module_example.py
@@ -3,12 +3,17 @@ import time
 example_variable = True
 
 
-def start():
+def start(card_id, card_data):
     """
     Called when this module should start executing.
 
     If there is an error starting this module, this function
-    should throw an appropriate exception
+    should throw an appropriate exception.
+
+    This function is run on a background thread, and should not
+    return until the module is finished running -- which could be
+    an indefinite amount of time until stop() is called, such
+    as a song on loop.
     """
     while example_variable:
         print "Hello World"
