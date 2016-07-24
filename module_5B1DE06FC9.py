3cf0aa8db8def938b1de2624cbd3bd2831227d78
Brett van Zuiden <brettcvz@gmail.com>
2016-07-23 22:21:00 -0700

    Handling IO the proper way

--- module_5B1DE06FC9.py
+++ module_5B1DE06FC9.py
@@ -1,16 +1,21 @@
 import module
 
 import subprocess
+import os.path
 
 
 class PandoraModule(module.Module):
     def run(self):
         super(PandoraModule, self).run()
         self.process = subprocess.Popen(["pianobar"])
-        #Switching stations
-        subprocess.call(["echo", "-n", 's', ">", "~/.config/pianobar/ctl"])
-        #to the station matching "Magnets"
-        subprocess.call(["echo", "Magnets", ">", "~/.config/pianobar/ctl"])
+
+        ctl_path = "~/.config/pianobar/ctl"
+        ctl_path = os.path.abspath(os.path.expanduser(ctl_path))
+        with open(ctl_path, "wb") as ctl:
+            #Switching stations
+            subprocess.call(["echo", "-n", 's'], stdout=ctl)
+            #to the station matching "Magnets"
+            subprocess.call(["echo", "Magnets"], stdout=ctl)
 
     def quit(self):
         self.process.terminate()
