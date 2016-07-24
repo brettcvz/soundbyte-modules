9d8c234beded11003c7c40e23d5b4567ea35b213
Brett van Zuiden <brettcvz@gmail.com>
2016-07-24 00:38:17 -0700

    Added some mp3 bits

---
+++ module_8804E84327.py
@@ -0,0 +1,31 @@
+import module
+
+import subprocess
+import os.path
+
+
+class Mp3Module(module.Module):
+    """
+    Plays campsite ambient music (for DnD)
+    """
+
+    root_dir = "soundbyte-modules"
+    song = "dnd_campsite.mp3"
+    loop = True
+
+    def filepath(self, fp):
+        return os.path.abspath(os.path.join(self.root_dir, "files", fp))
+
+    def run(self):
+        super(Mp3Module, self).run()
+        if self.loop:
+            self.process = subprocess.Popen(["mpg123", "--loop", "-1", self.filepath(self.song)])
+        else:
+            self.process = subprocess.Popen(["mpg123", self.filepath(self.song)])
+
+    def quit(self):
+        self.process.terminate()
+        super(Mp3Module, self).quit()
+
+start = Mp3Module.start
+stop = Mp3Module.stop
