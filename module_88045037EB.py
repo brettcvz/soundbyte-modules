5169e150b9fb7d8be9515b13a1d3b6d1f770df42
Brett van Zuiden <brettcvz@gmail.com>
2016-07-23 23:46:58 -0700

    Minor bugfixes

--- module_88045037EB.py
+++ module_88045037EB.py
@@ -1,16 +1,25 @@
 import module
 
 import subprocess
+import os.path
 
 
 class ConfigurationModule(module.Module):
     """
     Broadcasts a wireless access point and sets up a configuration server
     """
+
+    root_dir = "soundbyte-modules"
+    ap_config = "hostapd_ap.conf"
+    message = "access_point_created.wav"
+
+    def filepath(self, fp):
+        return os.path.abspath(os.path.join(self.root_dir, "files", fp))
+
     def run(self):
         super(ConfigurationModule, self).run()
-        self.ap_process = subprocess.Popen(["hostapd", "soundbyte-modules/files/hostapd_ap.conf"])
-        subprocess.call(["aplay", "-q", "soundbyte-modules/files/access_piont_created.wav"])
+        self.ap_process = subprocess.Popen(["hostapd", self.filepath(self.ap_config)])
+        subprocess.call(["aplay", "-q", self.filepath(self.message)])
 
     def quit(self):
         self.ap_process.terminate()
